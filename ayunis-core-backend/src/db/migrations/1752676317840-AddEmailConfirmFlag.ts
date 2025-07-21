e393df6e54f60cf967bb23e93e621db5e66be2d5
Daniel Benner <daniel@devbydaniel.com>
2025-07-21 14:05:29 +0200

    fix: fix migraiton

--- ayunis-core-backend/src/db/migrations/1752676317840-AddEmailConfirmFlag.ts
+++ ayunis-core-backend/src/db/migrations/1752676317840-AddEmailConfirmFlag.ts
@@ -1,18 +1,15 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddEmailConfirmFlag1752676317840 implements MigrationInterface {
-    name = 'AddEmailConfirmFlag1752676317840'
+  name = 'AddEmailConfirmFlag1752676317840';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP INDEX "public"."idx_messages_created_at"`);
-        await queryRunner.query(`DROP INDEX "public"."idx_messages_thread_created"`);
-        await queryRunner.query(`ALTER TABLE "users" ADD "emailVerified" boolean NOT NULL DEFAULT true`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "users" ADD "emailVerified" boolean NOT NULL DEFAULT true`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "emailVerified"`);
-        await queryRunner.query(`CREATE INDEX "idx_messages_thread_created" ON "messages" ("createdAt", "threadId") `);
-        await queryRunner.query(`CREATE INDEX "idx_messages_created_at" ON "messages" ("createdAt") `);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "emailVerified"`);
+  }
 }
