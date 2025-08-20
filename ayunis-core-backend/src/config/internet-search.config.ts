bb0363855a831719c7eafe01b15663f88a7e1570
Daniel Benner <daniel@devbydaniel.com>
2025-08-20 14:00:24 +0200

    fix: provide internet search tool only if available

--- ayunis-core-backend/src/config/internet-search.config.ts
+++ ayunis-core-backend/src/config/internet-search.config.ts
@@ -1,6 +1,10 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('internetSearch', () => ({
+  isAvailable:
+    process.env.BRAVE_SEARCH_URL &&
+    process.env.BRAVE_SEARCH_API_KEY &&
+    process.env.BRAVE_SEARCH_API_KEY !== '',
   brave: {
     url: process.env.BRAVE_SEARCH_URL,
     apiKey: process.env.BRAVE_SEARCH_API_KEY,
