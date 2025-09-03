d18d72b272a96139cc32bd0789534e45880083d4
Max Schluer <mail@maximilian-schluer.com>
2025-09-03 20:51:31 +0200

    add representative tests

--- ayunis-core-e2e-ui-tests/cypress/pages/components/sidebar.component.ts
+++ ayunis-core-e2e-ui-tests/cypress/pages/components/sidebar.component.ts
@@ -1,9 +1,24 @@
-class Navigation {
+class Sidebar {
 	rootLocator = `div[data-testid="sidebar"]`;
 
 	get buttonMenu() {
 		return cy.get(`${this.rootLocator} button[data-testid="menu"]`);
 	}
+
+	get chats() {
+		return cy.get(`${this.rootLocator} li[data-testid="chat"]`);
+	}
+
+	// Helpers
+	dropdownMenuTriggerForChat(title: string) {
+		return this.chats.contains(title).within(() => {
+			cy.get(`button[data-testid="dropdown-menu-trigger"]`).invoke('show')
+		});
+	}
+	forceClickDropdownMenuTriggerForChat(title: string) {
+		this.chats.contains(title).closest(`li[data-testid="chat"]`).find(`button[data-testid="dropdown-menu-trigger"]`).click({ force: true });
+	}
+
 }
 
-export default new Navigation();
+export default new Sidebar();
