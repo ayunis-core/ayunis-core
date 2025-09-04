adac304fffcd494b5324a71fe36395201faf0e83
Max Schluer <mail@maximilian-schluer.com>
2025-09-04 14:15:50 +0200

    remove obsolete test

--- ayunis-core-e2e-ui-tests/cypress/e2e/login.cy.ts
+++
@@ -1,7 +0,0 @@
-import { E2eTest01 } from '@data/accounts/E2eTest01.account';
-
-describe('Login Functionality', () => {
-	it('allows login for a registered user', () => {
-		cy.login(E2eTest01.email, E2eTest01.password, E2eTest01.username);
-	});
-});
