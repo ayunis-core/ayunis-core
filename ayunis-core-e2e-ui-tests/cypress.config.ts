eb08a7550b840e88d8fb7ae0322057c5dd372c47
Max Schluer <mail@maximilian-schluer.com>
2025-09-08 16:08:55 +0200

    resolved discussions during review

--- ayunis-core-e2e-ui-tests/cypress.config.ts
+++ ayunis-core-e2e-ui-tests/cypress.config.ts
@@ -3,5 +3,16 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
 	e2e: {
 		baseUrl: process.env['CYPRESS_BASE_URL'] || 'http://localhost:3000',
+		setupNodeEvents(on, config) {
+			on('before:run', async () => {
+				// ENV Sanity Check
+				const requiredEnv = [ `TEST_USER_PASSWORD` ];
+				for (const key of requiredEnv) {
+					if(!process.env[key]) {
+						throw new Error(`${key} is not defined. The following Environment Variables need to be set: ${requiredEnv}`);
+					}
+				}
+			});
+		}
 	},
 });
--- ayunis-core-e2e-ui-tests/cypress/e2e/login.cy.ts
+++ ayunis-core-e2e-ui-tests/cypress/e2e/login.cy.ts
@@ -1,10 +1,10 @@
-import { E2eTest01 } from '@data/accounts/E2eTest01.account';
+import { E2eTestGeneral } from '@data/accounts/E2eTest01.account';
 import ChatsPage from '../pages/chats.page';
 
 describe('Login Functionality', () => {
 	it('allows login for a registered user', () => {
-		cy.login(E2eTest01.email, E2eTest01.password);
+		cy.login(E2eTestGeneral.email, process.env.[`TEST_USER_PASSWORD`] ?? "");
 
-		ChatsPage.navigation.buttonMenu.should(`contain.text`, E2eTest01.username);
+		ChatsPage.navigation.buttonMenu.should(`contain.text`, E2eTestGeneral.username);
 	});
 });
