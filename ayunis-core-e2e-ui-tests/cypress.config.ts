e5b0f8c9ad74ac12be1f054b62d93d2981e1cba9
Max Schluer <mail@maximilian-schluer.com>
2025-09-08 16:26:23 +0200

    Fix linter offense

--- ayunis-core-e2e-ui-tests/cypress.config.ts
+++ ayunis-core-e2e-ui-tests/cypress.config.ts
@@ -3,16 +3,18 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
 	e2e: {
 		baseUrl: process.env['CYPRESS_BASE_URL'] || 'http://localhost:3000',
-		setupNodeEvents(on, config) {
+		setupNodeEvents(on) {
 			on('before:run', async () => {
 				// ENV Sanity Check
-				const requiredEnv = [ `TEST_USER_PASSWORD` ];
+				const requiredEnv = [`TEST_USER_PASSWORD`];
 				for (const key of requiredEnv) {
-					if(!process.env[key]) {
-						throw new Error(`${key} is not defined. The following Environment Variables need to be set: ${requiredEnv}`);
+					if (!process.env[key]) {
+						throw new Error(
+							`${key} is not defined. The following Environment Variables need to be set: ${requiredEnv}`,
+						);
 					}
 				}
 			});
-		}
+		},
 	},
 });
