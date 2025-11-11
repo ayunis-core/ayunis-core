b5fbfa0358e26a6f734e0c5b8356ec8acbdc9954
Daniel Benner <daniel@devbydaniel.com>
2025-11-11 09:38:40 +0100

    fix: postpone superadmin migration

--- ayunis-core-backend/src/db/migrations/1762850230665-AddSuperAdmin.ts
+++ ayunis-core-backend/src/db/migrations/1762850230665-AddSuperAdmin.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class AddSuperAdmin1762839387746 implements MigrationInterface {
-    name = 'AddSuperAdmin1762839387746'
+export class AddSuperAdmin1762850230665 implements MigrationInterface {
+    name = 'AddSuperAdmin1762850230665'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TYPE "public"."users_systemrole_enum" AS ENUM('customer', 'super_admin')`);
